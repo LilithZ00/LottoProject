9522a4a77cc672dfbc8985d553a8a7cb89197465
LilithZ007 <thitiwat.tiw17@gmail.com>
2024-08-28 20:00:40 +0700

    Pro&Wallet

---
+++ lib/pages/ProfilePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Profilepage extends StatelessWidget {
+  const Profilepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
