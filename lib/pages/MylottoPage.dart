8a02b78c4e7b3a86ff7b80d5b180f3960a41a875
LilithZ007 <thitiwat.tiw17@gmail.com>
2024-08-28 21:21:17 +0700

    eiei

--- lib/pages/MylottoPage.dart
+++ lib/pages/MylottoPage.dart
@@ -7,7 +7,9 @@ class Mylottopage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: Container(
+        
+      ),
     );
   }
 }
