e5c7655a9e8036faf8517f21b4b9e51ae4507619
LilithZ007 <thitiwat.tiw17@gmail.com>
2024-08-29 21:07:29 +0700

    addmin

---
+++ lib/pages/AdminPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Adminpage extends StatelessWidget {
+  const Adminpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        
+      ),
+    );
+  }
+}
