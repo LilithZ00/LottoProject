75f2a921b4e2e97ecf9ff2bc6d0fc76dc6bf17d2
LilithZ007 <thitiwat.tiw17@gmail.com>
2024-09-01 16:03:05 +0700

    แก้แอดมิน

--- lib/pages/AdminPage.dart
+++ lib/pages/AdminPage.dart
@@ -16,8 +16,8 @@ class _AdminPageState extends State<AdminPage> {
   // สร้างเลขสุ่ม 6 หลัก
   String generateRandomNumber() {
     Random random = Random();
-    int randomNumber = random.nextInt(900000) + 1; 
-    return randomNumber.toString().padLeft(6, '0'); 
+    int randomNumber = random.nextInt(900000) + 1;
+    return randomNumber.toString().padLeft(6, '0');
   }
 
   // แสดงตัวเลขแบบมีช่องว่างระหว่างตัวเลข
@@ -43,195 +43,191 @@ class _AdminPageState extends State<AdminPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFFE1BEE7), Color(0xFFBA68C8)],
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
           ),
-        ),
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 40), 
-            const Text(
-              'Admin', 
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'รางวัลที่ 1', 
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0), 
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: buildSpacedNumber(prizeNumbers[0]), // แสดงหมายเลขรางวัลที่ 1
-            ),
-            const SizedBox(height: 20),
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Column(
-                      children: [
-                        const Text(
-                          'รางวัลที่ 2', 
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 5),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0), 
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: buildSpacedNumber(prizeNumbers[1]), // แสดงหมายเลขรางวัลที่ 2
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        const Text(
-                          'รางวัลที่ 3',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 5),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0), 
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: buildSpacedNumber(prizeNumbers[2]), // แสดงหมายเลขรางวัลที่ 3
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Column(
-                      children: [
-                        const Text(
-                          'รางวัลที่ 4',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 5),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0), 
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: buildSpacedNumber(prizeNumbers[3]), // แสดงหมายเลขรางวัลที่ 4
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        const Text(
-                          'รางวัลที่ 5', 
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 5),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0), 
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: buildSpacedNumber(prizeNumbers[4]), // แสดงหมายเลขรางวัลที่ 5
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const Spacer(), // ช่องว่างระหว่างรายการรางวัลและปุ่มด้านล่าง
-            ElevatedButton(
-              onPressed: areButtonsDisabled ? null : drawPrizes, // ปุ่มกดเพื่อออกผลรางวัลจากที่ขายไป
-              style: ElevatedButton.styleFrom(
-                backgroundColor: areButtonsDisabled ? Colors.grey : Colors.black,
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-              ),
-              child: const Text(
-                'ออกผลรางวัลจากที่ขายไป',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: areButtonsDisabled ? null : drawPrizes, // ปุ่มกดเพื่อออกผลรางวัลจากทั้งหมด
-              style: ElevatedButton.styleFrom(
-                backgroundColor: areButtonsDisabled ? Colors.grey : Colors.black,
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-              ),
-              child: const Text(
-                'ออกผลรางวัลจากทั้งหมด',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: showResetDialog, // ปุ่มรีเซ็ตระบบ
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-              ),
-              child: const Text(
-                'รีเซ็ตระบบ',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: 
-                 () => loginPage(context), 
-              child: const Text(
-                'ออกจากระบบ',
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 40),
+              const Text(
+                'Admin',
                 style: TextStyle(
-                  fontSize: 16,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                   color: Colors.black,
-                  decoration: TextDecoration.underline,
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-          ],
+              const SizedBox(height: 10),
+              const Text(
+                'รางวัลที่ 1',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: buildSpacedNumber(prizeNumbers[0]),
+              ),
+              const SizedBox(height: 20),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Column(
+                        children: [
+                          const Text(
+                            'รางวัลที่ 2',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: buildSpacedNumber(prizeNumbers[1]),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            'รางวัลที่ 3',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: buildSpacedNumber(prizeNumbers[2]),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Column(
+                        children: [
+                          const Text(
+                            'รางวัลที่ 4',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: buildSpacedNumber(prizeNumbers[3]),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            'รางวัลที่ 5',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: buildSpacedNumber(prizeNumbers[4]),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: areButtonsDisabled ? null : drawPrizes,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: areButtonsDisabled ? Colors.grey : Colors.black,
+                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                ),
+                child: const Text(
+                  'ออกผลรางวัลจากที่ขายไป',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: areButtonsDisabled ? null : drawPrizes,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: areButtonsDisabled ? Colors.grey : Colors.black,
+                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                ),
+                child: const Text(
+                  'ออกผลรางวัลจากทั้งหมด',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: showResetDialog,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                ),
+                child: const Text(
+                  'รีเซ็ตระบบ',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () => loginPage(context),
+                child: const Text(
+                  'ออกจากระบบ',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
@@ -241,19 +237,19 @@ class _AdminPageState extends State<AdminPage> {
   void drawPrizes() {
     setState(() {
       for (int i = 0; i < prizeNumbers.length; i++) {
-        prizeNumbers[i] = generateRandomNumber(); // สุ่มหมายเลขรางวัล
+        prizeNumbers[i] = generateRandomNumber();
       }
-      areButtonsDisabled = true; // ปิดการใช้งานปุ่มหลังจากกดแล้ว
+      areButtonsDisabled = true;
     });
   }
 
   // รีเซ็ตหมายเลขรางวัล
   void resetPrizes() {
     setState(() {
-      prizeNumbers = List.filled(5, '000000'); // รีเซ็ตหมายเลขรางวัลกลับเป็น 000000
-      areButtonsDisabled = false; // เปิดการใช้งานปุ่มอีกครั้ง
+      prizeNumbers = List.filled(5, '000000');
+      areButtonsDisabled = false;
     });
-    Navigator.pop(context); // ปิด Dialog หลังจากรีเซ็ต
+    Navigator.pop(context);
   }
 
   // ยืนยันการรีเซ็ต
@@ -262,18 +258,18 @@ class _AdminPageState extends State<AdminPage> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('รีเซ็ตระบบ'), 
-          content: const Text('คุณแน่ใจหรือไม่ว่าต้องการรีเซ็ต?'), 
+          title: const Text('รีเซ็ตระบบ'),
+          content: const Text('คุณแน่ใจหรือไม่ว่าต้องการรีเซ็ต?'),
           actions: <Widget>[
             TextButton(
               child: const Text('ยกเลิก'),
               onPressed: () {
-                Navigator.of(context).pop(); // ปิด Dialog ถ้ากด ยกเลิก
+                Navigator.of(context).pop();
               },
             ),
             TextButton(
               child: const Text('ตกลง'),
-              onPressed: resetPrizes, // รีเซ็ตระบบเมื่อกด ตกลง
+              onPressed: resetPrizes,
             ),
           ],
         );
@@ -285,13 +281,7 @@ class _AdminPageState extends State<AdminPage> {
   loginPage(BuildContext context) {
     Navigator.pop(
       context,
-      MaterialPageRoute(builder: (context) => LoginPage()), 
+      MaterialPageRoute(builder: (context) => LoginPage()),
     );
   }
 }
-
-// void main() {
-//   runApp(const MaterialApp(
-//     home: AdminPage(),
-//   ));
-// }
